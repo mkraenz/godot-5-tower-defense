14c7d79ed15e7c7be76c2b9e47538dfe2fbf4840
Mirco Kraenz <kraenzmirco@gmail.com>
2022-12-05 17:46:26 +0900

    feat: money anim but with warning that timer interval is too small..

--- UI/HUD.gd
+++ UI/HUD.gd
@@ -5,16 +5,13 @@ onready var money = $MoneyHStack
 
 func _ready() -> void:
     nexus.connect("money_changed", self, "_on_money_changed")
-    update_money()
+    money.init_money(nexus.money)
 
 func _on_money_changed(data: Dictionary) -> void:
-    update_money()
+    money.update_money(nexus.money)
     var has_spent_money = data["diff"] < 0
     if has_spent_money:
         pass
         # todo play some sound
     else: # i.e. we received money
         pass
-
-func update_money():
-    money.update_money(nexus.money)
--- UI/MoneyHStack.gd
+++ UI/MoneyHStack.gd
@@ -1,6 +1,23 @@
 extends HBoxContainer
 
 onready var label = $Label
+onready var timer = $Timer # TODO probably better to use lerp (with cubic interpolation with fixed time)
+
+var target_money = 0.0
+var current_money = 0.0
+
+func _on_Timer_timeout() -> void:
+    current_money += sign(target_money - current_money)
+    label.text = str(current_money)
+    if target_money != current_money:
+        timer.start()
+
+func init_money(amount: float) -> void:
+    target_money = amount
+    current_money = amount
+    label.text = str(current_money)
 
 func update_money(amount: float):
-    label.text = str(amount)
+    target_money = amount
+    if timer.is_stopped():
+        timer.start()
