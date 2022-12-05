b12c74383790ca9b51013eb617fd578b6b5379f6
Mirco Kraenz <kraenzmirco@gmail.com>
2022-12-05 19:13:40 +0900

    refactor: simplified lerp

--- UI/MoneyHStack.gd
+++ UI/MoneyHStack.gd
@@ -4,21 +4,18 @@ onready var label = $Label
 
 var target_money = 0.0
 var current_money = 0.0
-var weight := 0.0
 const anim_speed := 1
 
 func _ready() -> void:
     set_physics_process(false)
 
 func _physics_process(delta: float) -> void:
-    weight += delta * anim_speed
-    weight = clamp(weight, 0, 1)
+    var weight = clamp(delta * anim_speed, 0, 1)
     current_money = ceil(lerp(current_money, target_money, weight))
 
     label.text = str(current_money)
     
     if current_money == target_money:
-        weight = 0.0
         set_physics_process(false)
 
 func init_money(amount: float) -> void:
