fd92f0510d71220689a62607a524c2a39ca2447c
Mirco Kraenz <kraenzmirco@gmail.com>
2022-12-05 20:24:51 +0900

    feat: improve camera movement

--- Cam.gd
+++ Cam.gd
@@ -1,13 +1,12 @@
 extends Camera2D
 
-var speed := 10.0
+var speed := 5.0
 const follow_speed := 5.0
 onready var initial_position = position
 onready var bottom_right = Vector2(limit_right - initial_position.x, limit_top - initial_position.y)
 onready var target_position = position
 
-
-func _unhandled_input(_event: InputEvent) -> void:	
+func _physics_process(delta: float) -> void:
 	var input_vec = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 	if input_vec != Vector2.ZERO:
 		target_position += input_vec * speed
@@ -15,11 +14,4 @@ func _unhandled_input(_event: InputEvent) -> void:
 		target_position.y = max(target_position.y, initial_position.y)
 		target_position.x = min(target_position.x, bottom_right.x)
 		target_position.y = min(target_position.y, bottom_right.y)
-
-func _physics_process(delta: float) -> void:
 	position = position.linear_interpolate(target_position, delta * follow_speed)
-
-
-# cam position is centered in the screen, otherwise it won't move... Thus correcting by the initial offset.
-func zero_based_position() -> Vector2:
-	return position - initial_position
